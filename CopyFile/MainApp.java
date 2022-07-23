8e37fb0d634768312f61a39db0c7f15530d23429
AL807S <alyjak@gmail.com>
2022-07-23 10:47:05 +0200

    update: MainApp

--- CopyFile/MainApp.java
+++ CopyFile/MainApp.java
@@ -8,8 +8,8 @@ public class MainApp implements common {
     //region main
     public static void main(String[] args) throws IOException {
 
-//        CopyFilesUsingNIO.copyFilesUsingNIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
-        CopyFilesUsingIO.copyFilesUsingIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
+        CopyFilesUsingNIO.copyFilesUsingNIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
+//        CopyFilesUsingIO.copyFilesUsingIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
 //        CopyFileUsingIO2.copyFileUsingIO2();
 //        DeleteFile.cleanUpFiles(new File(PATH_TO_DESTINATION));
 
