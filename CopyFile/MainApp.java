157d9c639e8c682c5bade347d0495b53bbbf6b82
alyjak <alyjak@gmail.com>
2022-07-25 12:36:56 +0200

    commit after fetch

--- CopyFile/MainApp.java
+++ CopyFile/MainApp.java
@@ -8,13 +8,16 @@ public class MainApp implements common {
     //region main
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Działam na hotfix");
-        System.out.println("Wrocilem na hotfix-a");
         System.out.println("A tu jade na masterze!");
         System.out.println("Robie nowy branch na masterze");
         System.out.println("Robie nowy branch na masterze2");
         System.out.println("Zostawiam zmiany i spier.alam stad");
         System.out.println("Zostawiam zmiany i spier.alam stad2");
+        System.out.println("Działam na hotfix");
+        System.out.println("A teraz utw. quickFix-a");
+        System.out.println("Wrocilem na hotfix-a");
+        System.out.println("A tu jade na masterze!");
+        System.out.println("Robie nowy branch na masterze");
         CopyFilesUsingNIO.copyFilesUsingNIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
 //        CopyFilesUsingIO.copyFilesUsingIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
 //        CopyFileUsingIO2.copyFileUsingIO2();
