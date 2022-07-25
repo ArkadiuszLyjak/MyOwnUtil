cbee3a66888fa6a15ed3a79209b0dd28340567fe
alyjak <alyjak@gmail.com>
2022-07-25 13:21:36 +0200

    rebased : first change on quickFix

--- CopyFile/MainApp.java
+++ CopyFile/MainApp.java
@@ -23,6 +23,7 @@ public class MainApp implements common {
         System.out.println("Działam na qucikModHot");
         System.out.println("Działam na qucikMod");
         System.out.println("Działam na qucikMod2");
+        System.out.println("Działam na quickFix");
         CopyFilesUsingNIO.copyFilesUsingNIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
 //        CopyFilesUsingIO.copyFilesUsingIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
 //        CopyFileUsingIO2.copyFileUsingIO2();
