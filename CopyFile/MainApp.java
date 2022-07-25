3ee85f6c8a55c7439db1890a2a180599bf4720f5
alyjak <alyjak@gmail.com>
2022-07-25 13:17:46 +0200

    first change on hotFix

--- CopyFile/MainApp.java
+++ CopyFile/MainApp.java
@@ -9,7 +9,7 @@ public class MainApp implements common {
     public static void main(String[] args) throws IOException {
 
         System.out.println("Działam na hotfix");
-        CopyFilesUsingNIO.copyFilesUsingNIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
+//        CopyFilesUsingNIO.copyFilesUsingNIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
 //        CopyFilesUsingIO.copyFilesUsingIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
 //        CopyFileUsingIO2.copyFileUsingIO2();
 //        DeleteFile.cleanUpFiles(new File(PATH_TO_DESTINATION));
