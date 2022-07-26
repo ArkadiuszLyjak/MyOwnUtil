75808a5dca1dd68b5daed780462499ccb4f16dcf
alyjak <alyjak@gmail.com>
2022-07-26 18:09:05 +0200

    rebased: created new parallel branch for testing rebase

--- CopyFile/MainApp.java
+++ CopyFile/MainApp.java
@@ -20,6 +20,7 @@ public class MainApp implements common {
         System.out.println("Robie nowy branch na masterze");
         System.out.println("Działam na qucikMod");
         System.out.println("Działam na qucikMod2");
+        System.out.println("Działam na qucikModHot");
         CopyFilesUsingNIO.copyFilesUsingNIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
 //        CopyFilesUsingIO.copyFilesUsingIO(common.PATH_TO_SOURCE, common.PATH_TO_DESTINATION);
 //        CopyFileUsingIO2.copyFileUsingIO2();
